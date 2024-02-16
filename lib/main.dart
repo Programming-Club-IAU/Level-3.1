4405c5b5b54a84ad9d32e7f89ac9bc3bda72ae34
FarisAI24 <faris.alzayed24@gmail.com>
2024-02-16 22:21:07 +0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
   final int colorBackgroundTop = 0xFFF44336;
   final int colorBackgroundBottom = 0xFF212133;
+  final int colorLabel = 0xCDFFFBFB;
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +34,22 @@ class HomePage extends StatelessWidget {
               color: Color(colorBackgroundBottom),
               child: Stack(
                 children: [
+                  const Center(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Padding(
+                        padding: EdgeInsets.only(bottom: 20.0),
+                        child: Text(
+                          '@UserName123',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                   Container(
                     height: MediaQuery.of(context).size.height * 0.20,
                     width: MediaQuery.of(context).size.width,
@@ -94,6 +111,70 @@ class HomePage extends StatelessWidget {
               height: MediaQuery.of(context).size.height * 0.60,
               width: MediaQuery.of(context).size.width,
               color: Color(colorBackgroundBottom),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        labelText: 'Name',
+                        labelStyle: TextStyle(color: Color(colorLabel)),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(color: Color(colorLabel)),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        labelText: 'Phone Number',
+                        labelStyle: TextStyle(color: Color(colorLabel)),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        labelText: 'Nationality',
+                        labelStyle: TextStyle(color: Color(colorLabel)),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            Color(colorBackgroundTop), // background
+                        foregroundColor: Colors.white,
+                        minimumSize: const Size(200, 60), // foreground
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Save Profile',
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
