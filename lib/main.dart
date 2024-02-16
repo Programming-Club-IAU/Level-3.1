80b0dac8ee77b1568552829fb59756c0eec098ae
FarisAI24 <faris.alzayed24@gmail.com>
2024-02-16 20:59:57 +0300

    Updated files

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,92 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  final int colorBackgroundTop = 0xFFF44336;
+  final int colorBackgroundBottom = 0xFF212133;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.40,
+              width: MediaQuery.of(context).size.width,
+              color: Color(colorBackgroundBottom),
+              child: Stack(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.20,
+                    width: MediaQuery.of(context).size.width,
+                    color: Color(colorBackgroundTop),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: CircleAvatar(
+                      radius: 110,
+                      backgroundColor: Color(colorBackgroundBottom),
+                    ),
+                  ),
+                  const Align(
+                    alignment: Alignment.center,
+                    child: CircleAvatar(
+                      radius: 100,
+                      backgroundColor: Colors.transparent,
+                      foregroundColor: Colors.transparent,
+                      child: ClipOval(
+                        child: Image(
+                          image: AssetImage('assets/images/profilepic.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 100,
+                    right: 115,
+                    child: Stack(
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: Color(colorBackgroundBottom),
+                        ),
+                        const Positioned(
+                          bottom: 5,
+                          right: 5,
+                          child: CircleAvatar(
+                            radius: 25,
+                            backgroundColor: Colors.transparent,
+                            foregroundColor: Colors.transparent,
+                            child: ClipOval(
+                              child: Image(
+                                image:
+                                    AssetImage('assets/images/edit-icon.png'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.60,
+              width: MediaQuery.of(context).size.width,
+              color: Color(colorBackgroundBottom),
+            ),
+          ],
         ),
       ),
     );
